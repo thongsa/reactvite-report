ffb9cf85be9b2b176180683c661075b19867cc92
thongsa <somphongts@gmail.com>
2024-09-25 17:01:59 +0700

    update_25092024

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import ReportComponent from './Report'
+import ReportComponent1 from './Report1'
+import ExportExcelWithImage from './ReportImg'
+// import ExportXMLToExcel from './ReportbyXML'
 function App() {
   // const [count, setCount] = useState(0)
-
   return (
     <>
       <ReportComponent/>
+      <hr/><br></br>
+      <ReportComponent1/>
+      <hr/><br></br>
+      <ExportExcelWithImage/>
     </>
   )
 }
